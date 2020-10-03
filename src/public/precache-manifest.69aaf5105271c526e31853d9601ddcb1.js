21886971552a414033c810f988d81e925b7725fc
Mariano <mariano.14th@gmail.com>
2020-10-03 17:44:16 -0600

    v1 calendar app

---
+++ src/public/precache-manifest.69aaf5105271c526e31853d9601ddcb1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a66daf68a6a7ad1424dc2c6902d289f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "28ccf1ccd6d793ede2d5",
+    "url": "/static/css/2.dabbbe1b.chunk.css"
+  },
+  {
+    "revision": "ce556a72693c5ac0ac55",
+    "url": "/static/css/main.c1a71f23.chunk.css"
+  },
+  {
+    "revision": "28ccf1ccd6d793ede2d5",
+    "url": "/static/js/2.032bcb6e.chunk.js"
+  },
+  {
+    "revision": "590af14d9bb9b4b1926797b2a32e33a1",
+    "url": "/static/js/2.032bcb6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce556a72693c5ac0ac55",
+    "url": "/static/js/main.8ebd9cc4.chunk.js"
+  },
+  {
+    "revision": "7e829d7f4f2411c25d99",
+    "url": "/static/js/runtime-main.8c2230a5.js"
+  }
+]);
